d3d94acfcf55a5039f9eb365ba19901e2ba1aee9
dallin <dallinrupper@gmail.com>
2023-05-01 22:27:07 -0600

    only touch up left

--- cs-1810-final-project-dallinrupper/movie-details.js
+++ cs-1810-final-project-dallinrupper/movie-details.js
@@ -1,36 +1,53 @@
-document.addEventListener('DOMContentLoaded',() =>
-{
-function loadMovieDetails() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const movieIndex = parseInt(urlParams.get('index'));
-  
-  if (movieIndex) {
-    const allMovies = JSON.parse(localStorage.getItem('allMovies'));
-    const movie = allMovies[movieIndex];
-    
-    if (movie) {
-      displayMovieDetails(movie);
-    } else {
-      console.error('Movie not found');
-    }
-  } else {
-    console.error('Invalid movie index');
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
+  function loadMovieDetails() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const movieIndex = parseInt(urlParams.get('index'));
 
-function displayMovieDetails(movie) {
-  const movieDetailsContainer = document.getElementById('movie-details');
-  
-  const defaultImageUrl = 'https://via.placeholder.com/300x450.png?text=No+Image+Available';
-  const imageUrl = movie.backdropURLs && movie.backdropURLs.original
-  ? movie.backdropURLs.original
-  : defaultImageUrl;
-  
-  movieDetailsContainer.innerHTML = `
+    if (movieIndex) {
+      const allMovies = JSON.parse(localStorage.getItem('allMovies'));
+      const movie = allMovies[movieIndex];
+
+      if (movie) {
+        displayMovieDetails(movie);
+      } else {
+        console.error('Movie not found');
+      }
+    } else {
+      console.error('Invalid movie index');
+    }
+  }
+
+  function displayMovieDetails(movie) {
+    const movieDetailsContainer = document.getElementById('movie-details');
+
+    const defaultImageUrl = 'https://via.placeholder.com/300x450.png?text=No+Image+Available';
+    const imageUrl = movie.backdropURLs && movie.backdropURLs.original
+      ? movie.backdropURLs.original
+      : defaultImageUrl;
+    const imageUrl2 = movie.posterURLs && movie.posterURLs.original
+    ? movie.posterURLs.original
+    : defaultImageUrl;
+
+      let castMembersHtml = '';
+      for (let i = 0; i < 6 && i < movie.cast.length; i++) {
+      castMembersHtml += (i < movie.cast.length - 1) ? `<span>${movie.cast[i]}, </span>` : `<span>${movie.cast[i]}</span>`;
+      }
+
+      const youtubeVideoId = movie.youtubeTrailerVideoLink;
+
+    movieDetailsContainer.innerHTML = `
   <h1>${movie.title}</h1>
+  <div class="images-container">
   <img src="${imageUrl}" alt="${movie.title}" />
-  <p>${movie.description}</p>
-  `;
-}
-loadMovieDetails();
+  <img src="${imageUrl2}" alt="${movie.title}" />
+  </div>
+  <p>${movie.overview}</p>
+
+  <h2>Cast</h2>
+  <div class="cast-members">${castMembersHtml}</div>
+
+  <h2>Trailer</h2>
+  <a href="${youtubeVideoId}" target="_blank">Watch the trailer on YouTube</a>  `;
+  }
+  loadMovieDetails();
 })
--- cs-1810-final-project-dallinrupper/movie.js
+++ cs-1810-final-project-dallinrupper/movie.js
@@ -1,110 +1,138 @@
-const apiKey = 'fe07250b03msh81b8cdc278ac240p1694d9jsnd3554ffecf3a';
-const apiHost = 'streaming-availability.p.rapidapi.com';
-
-function fetchMovies(selectedGenre, selectedLanguage, cursor = null) {
-	const options = {
+class MovieFetcher 
+{
+	constructor(apiKey, apiHost) {
+	  this.apiKey = apiKey;
+	  this.apiHost = apiHost;
+	  this.pagesFetched = 0;
+	  this.maxPages = 15;
+	}
+  
+	fetchMovies(selectedGenre, selectedLanguage, checkedServices, cursor = null) {
+	  const options = {
 		method: 'GET',
 		headers: {
-			'X-RapidAPI-Key': apiKey,
-			'X-RapidAPI-Host': apiHost
-		}
-	};
-	const cursorQuery = cursor ? `&cursor=${encodeURIComponent(cursor)}` : '';
-
-	const apiUrl = `https://streaming-availability.p.rapidapi.com/v2/search/basic?country=us&service=netflix%2Cprime.buy%2Chulu.addon.hbo%2Cpeacock.free&output_language=en&type=all&genre=${selectedGenre}&language=${selectedLanguage}&${cursorQuery}`;
-	console.log("API URL:", apiUrl);
-
-	return fetch(apiUrl, options).then(response => response.json());
-}
-
-function getRandomMovies(movies, count) {
-	const randomMovies = [];
-	for (let i = 0; i < count; i++) {
+		  'X-RapidAPI-Key': this.apiKey,
+		  'X-RapidAPI-Host': this.apiHost,
+		},
+	  };
+	  const cursorQuery = cursor ? `&cursor=${encodeURIComponent(cursor)}` : '';
+  
+	  const apiUrl = `https://streaming-availability.p.rapidapi.com/v2/search/basic?country=us&service=${checkedServices}&output_language=${selectedLanguage}&type=all&genre=${selectedGenre}&language=${selectedLanguage}&${cursorQuery}`;
+	  console.log('API URL:', apiUrl);
+  
+	  return fetch(apiUrl, options).then(response => response.json());
+	}
+  
+	getRandomMovies(movies, count) {
+	  const randomMovies = [];
+	  for (let i = 0; i < count; i++) {
 		const randomIndex = Math.floor(Math.random() * movies.length);
 		randomMovies.push(movies[randomIndex]);
 		movies.splice(randomIndex, 1);
-	}
-	return randomMovies;
-}
-
-function filterMovies() {
-	const genreSelector = document.getElementById('genre-selector');
-	const languageSelector = document.getElementById('language-selector');
-
-	const selectedGenre = genreSelector.value || '18';
-	const selectedLanguage = languageSelector.value || 'en';
-
-	let allMovies = [];
-	let cursor = null;
-	let hasMore = true;
-	let pagesFetched = 0;
-	const maxPages = 15;
-
-	function fetchNextPage() {
-		if (pagesFetched >= maxPages) {
-			hasMore = false;
-		}
-		fetchMovies(selectedGenre, selectedLanguage, cursor)
-			.then(data => {
-				allMovies = allMovies.concat(data.result);
-				cursor = data.nextCursor;
-				hasMore = data.hasMore;
-
-				pagesFetched++;
-
-				if (hasMore && pagesFetched < maxPages) {
-					fetchNextPage();
-				} else {
-					const randomMovies = getRandomMovies(allMovies, 9);
-					displayMovies(randomMovies);
-				}
-			})
-			.catch(err => console.error(err));
+	  }
+	  return randomMovies;
 	}
 
-	fetchNextPage();
-}
-
-function displayMovies(moviesToDisplay) {
-	localStorage.setItem('allMovies', JSON.stringify(moviesToDisplay));
-  
-	const gridContainer = document.querySelector('.grid-container');
-	gridContainer.innerHTML = '';
-	const defaultImageUrl = 'https://via.placeholder.com/300x168.png?text=No+Image+Available';
-
-  
-	moviesToDisplay.forEach((movie, index) => {
-	  if (!movie) {
-		return;
+	toggleLoading(loading) {
+		const loadingElement = document.getElementById('loading');
+		loadingElement.style.display = loading ? 'block' : 'none';
 	  }
   
-	  const movieElement = document.createElement('div');
-	  movieElement.classList.add('movie');
+	filterMovies() {
+		const genreSelector = document.getElementById('genre-selector');
+		const languageSelector = document.getElementById('language-selector');
+		const streamingServices = Array.from(document.getElementsByName('streaming-service'));
+	  
+		const selectedGenre = genreSelector.value;
+		const selectedLanguage = languageSelector.value;
+	  
+		const checkedServices = streamingServices
+		  .filter(checkbox => checkbox.checked)
+		  .map(checkbox => checkbox.value)
+		  .join('%2C');
+	  
+		let allMovies = [];
+		let cursor = null;
+		let hasMore = true;
+		this.maxPages = 15;
+		this.pagesFetched = 0;
+	  
+		this.toggleLoading(true); // Show the loading image
+	  
+		const fetchNextPage = () => {
+		  if (this.pagesFetched >= this.maxPages) {
+			hasMore = false;
+		  }
+		  this.fetchMovies(selectedGenre, selectedLanguage, checkedServices, cursor)
+			.then(data => {
+			  allMovies = allMovies.concat(data.result);
+			  cursor = data.nextCursor;
+			  hasMore = data.hasMore;
+	  
+			  this.pagesFetched++;
+	  
+			  if (hasMore && this.pagesFetched < this.maxPages) {
+				fetchNextPage();
+			  } else {
+				const randomMovies = this.getRandomMovies(allMovies, 9);
+				this.displayMovies(randomMovies);
+				this.toggleLoading(false); // Hide the loading image
+			  }
+			})
+			.catch(err => {
+			  console.error(err);
+			  this.toggleLoading(false); // Hide the loading image in case of an error
+			});
+		};
+	  
+		fetchNextPage();
+	  }
   
-	  const imageUrl = movie.backdropURLs && movie.backdropURLs.original
-		? movie.backdropURLs.original
-		: defaultImageUrl;
+	displayMovies(moviesToDisplay) {
+	  localStorage.setItem('allMovies', JSON.stringify(moviesToDisplay));
   
-	  movieElement.innerHTML = `
-		<h2>${movie.title}</h2>
-		<img src="${imageUrl}" alt="${movie.title}" />
-	  `;
-	  addTitleClickEventListener(movieElement, index);
-	  gridContainer.appendChild(movieElement);
-	});
-  }
-
-document.addEventListener('DOMContentLoaded', () => {
-	document
+	  const gridContainer = document.querySelector('.grid-container');
+	  gridContainer.innerHTML = '';
+	  const defaultImageUrl = 'https://via.placeholder.com/300x168.png?text=No+Image+Available';
+  
+	  moviesToDisplay.forEach((movie, index) => {
+		if (!movie) {
+		  return;
+		}
+  
+		const movieElement = document.createElement('div');
+		movieElement.classList.add('movie');
+  
+		const imageUrl = movie.backdropURLs && movie.backdropURLs.original
+		  ? movie.backdropURLs.original
+		  : defaultImageUrl;
+  
+		  movieElement.innerHTML = `
+		  <h2>${movie.title}</h2>
+		  <img src="${imageUrl}" alt="${movie.title}" />
+		  `;
+		  this.addTitleClickEventListener(movieElement, index);
+		  gridContainer.appendChild(movieElement);
+		});
+	  }
+	
+	  addTitleClickEventListener(movieElement, movieIndex) {
+		const movieTitle = movieElement.querySelector('h2');
+		movieTitle.style.cursor = 'pointer';
+	
+		movieTitle.addEventListener('click', () => {
+		  window.location.href = `movie-details.html?index=${movieIndex}`;
+		});
+	  }
+	}
+	
+	document.addEventListener('DOMContentLoaded', () => {
+	  const apiKey = 'fe07250b03msh81b8cdc278ac240p1694d9jsnd3554ffecf3a';
+	  const apiHost = 'streaming-availability.p.rapidapi.com';
+	  const movieFetcher = new MovieFetcher(apiKey, apiHost);
+	
+	  document
 		.getElementById('search-button')
-		.addEventListener('click', filterMovies);
-});
-
-function addTitleClickEventListener(movieElement, movieIndex) {
-	const movieTitle = movieElement.querySelector('h2');
-	movieTitle.style.cursor = 'pointer';
-  
-	movieTitle.addEventListener('click', () => {
-	  window.location.href = `movie-details.html?index=${movieIndex}`;
+		.addEventListener('click', () => movieFetcher.filterMovies());
 	});
-  }
+  
